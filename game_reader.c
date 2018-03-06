c3e9e53d8c72eb3fa801d0c65f23e72573889b5d
alexmartin21 <35938543+alexmartin21@users.noreply.github.com>
2018-03-07 00:15:07 +0100

    librerias actualizadas fiera tifon torbellino

--- game_reader.c
+++ game_reader.c
@@ -12,11 +12,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include "game_reader.h"
-#include "types.h"
 #include "space.h"
 #include "command.h"/*Ya vienen en "game_reader.h"*/
 #include "game.h" /*Ya vienen en "game_reader.h"*/
-#include "time.h"/*Carga randomizada de objetos en casilla*/
 #include "set.h"
 
 #define DEBUG
